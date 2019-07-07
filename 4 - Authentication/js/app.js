556fd731402fd13937e10f2ca64e32b9521374fa
Wesley Feitosa <jwesleydasilva@gmail.com>
2019-07-06 22:40:53 -0300

    atualizando

--- 4 - Authentication/js/app.js
+++ 4 - Authentication/js/app.js
@@ -1,3 +1,6 @@
 function logout() {
-
+    // faço um logout do meu usuário, ou seja, saio da aplicação
+    firebase.auth().signOut().then(() => {
+        console.log('Usuário deslogou!');
+    })
 }
